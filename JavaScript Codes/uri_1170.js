056cc21037c2fe37f8fdfe76890e22ce2ab03177
Masum-Osman <ogkmasum@gmail.com>
2019-03-02 00:05:12 +0600

    Initaial commit of all codes

---
+++ JavaScript Codes/uri_1170.js
@@ -0,0 +1,12 @@
+function func() { 
+  
+    //Original Array 
+    var arr = [34, 234, 567, 4]; 
+    console.log(arr); 
+  
+    //Reversed array 
+    var new_arr = arr.reverse(); 
+     
+    console.log(new_arr); 
+} 
+func(); 
